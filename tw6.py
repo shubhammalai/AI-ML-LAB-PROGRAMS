4260fc878e5633dd9e62248b94751f8d3b1145c8
shubhammalai <37304657+shubhammalai@users.noreply.github.com>
2022-03-24 11:12:20 +0530

    Update tw6.py

--- tw6.py
+++ tw6.py
@@ -2,7 +2,7 @@
 
 import pandas as pd
 
-msg=pd.read_csv('Term6Dataset.csv',names=['message','label']) #Tabular form data
+msg=pd.read_csv('Term6Dataset.csv',names=['message','label']) 
 print('Total instances in the dataset:',msg.shape[0])
 msg['labelnum']=msg.label.map({'pos':1,'neg':0})
 X=msg.message
@@ -12,7 +12,7 @@ print('\nThe message and its label of first 5 instances are listed below')
 #X5, Y5 = X[0:5], msg.label[0:5]
 for x, y in zip(X[:5],Y[:5]):
     print(x,',',y)
-# Splitting the dataset into train and test data
+
 
 from sklearn.model_selection import train_test_split
 xtrain,xtest,ytrain,ytest=train_test_split(X,Y)
@@ -20,19 +20,17 @@ print('\nDataset is split into Training and Testing samples')
 print('Total training instances :', xtrain.shape[0])
 print('Total testing instances :', xtest.shape[0])
 
-# Output of count vectoriser is a sparse matrix
-# CountVectorizer - stands for 'feature extraction'
+
 from sklearn.feature_extraction.text import CountVectorizer
 count_vect = CountVectorizer()
-xtrain_dtm = count_vect.fit_transform(xtrain) #Sparse matrix
+xtrain_dtm = count_vect.fit_transform(xtrain) 
 xtest_dtm = count_vect.transform(xtest)
 print('\nTotal features extracted using CountVectorizer:',xtrain_dtm.shape[1])
 print('\nFeatures for first 5 training instances are listed below')
 
 df=pd.DataFrame(xtrain_dtm.toarray(),columns=count_vect.get_feature_names())
 print(df[0:5])#tabular representation
-#print(xtrain_dtm) #Same as above but sparse matrix representation
-# Training Naive Bayes (NB) classifier on training data.
+
 
 from sklearn.naive_bayes import MultinomialNB
 clf = MultinomialNB().fit(xtrain_dtm,ytrain)
